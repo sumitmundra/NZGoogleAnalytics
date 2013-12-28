205d1bd9d955fbee660307ce7e54d9d5cb5093c3
Bruno Tortato Furtado <bruno.furtado@gruponzn.com>
2013-12-28 18:09:06 -0200

    Removing unnecessary import

--- Example/Classes/NZAppDelegate.m
+++ Example/Classes/NZAppDelegate.m
@@ -7,12 +7,11 @@
 //
 
 #import "NZAppDelegate.h"
-#import "NZGoogleAnalytics.h"
 
 @implementation NZAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{    
+{
     // Override point for customization after application launch.
     return YES;
 }
